82b2fa82dcc84267b8a97e9d96aed3d692acd7b9
xianyuhekouchou <13669601103@139.com>
2025-08-13 20:23:48 +0800

    删库跑路

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-const app = createApp(App)
-//公共函数
-import res from '@/assets/gbfunc.js';
-app.config.globalProperties.$req = res;
-import 'vant/lib/index.css';
-import '@/assets/main.css'
-app.mount('#app')
